9c0fc1df40d54b6c7ead1ca691c3da1f3987ec2e
Yupopoi <ysy2001number1@docomo.ne.jp>
2025-03-05 03:43:44 +0900

    [Update] Enabled to start performing eye movements

--- Assets/Scripts/ResultAdapter/FaceAdaptationManager.cs
+++ Assets/Scripts/ResultAdapter/FaceAdaptationManager.cs
@@ -4,14 +4,20 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+using UnityEngine;
 using Mediapipe.Tasks.Vision.FaceLandmarker;
 
 namespace Mediapipe.Allocator
 {
     public class FaceAdaptationManager : AdaptationManagerBase<FaceLandmarkerResult>
     {
+        GameObject _faceObject; 
+
         LandmarksPacket _mouthPacket;
         MouthAdapter _mouthAdapter;
+
+        LandmarksPacket _eyePacket;
+        EyeAdapter _eyeAdapter;
 
         protected override void Start()
         {
@@ -19,8 +25,13 @@ namespace Mediapipe.Allocator
 
             GenerateLandmarksList(478);
 
+            _faceObject = _vrmObject.transform.Find("Face").gameObject;
+
             _mouthPacket = new(_landmarks, new int[13] { 13, 14, 306, 78, 311, 81, 402, 178, 17, 473, 468, 334, 105 });
-            _mouthAdapter = new(_vrmObject.transform.Find("Face").gameObject, _mouthPacket);
+            _mouthAdapter = new(_faceObject, _mouthPacket);
+
+            _eyePacket = new(_landmarks, new int[16] { 473, 468, 386, 374, 159, 145, 263, 362, 132, 33, 475, 477, 470, 472, 334, 105 });
+            _eyeAdapter = new(_faceObject, _eyePacket);
         }
 
         public override void ApplyMediapipeResult(FaceLandmarkerResult recognitionResult)
@@ -31,6 +42,7 @@ namespace Mediapipe.Allocator
             }
 
             _mouthAdapter.ForwardApply();
+            _eyeAdapter.ForwardApply();
         }
     }
 
